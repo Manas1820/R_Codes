739c6d075b2e6c8e555857b8c79c3126d63a3058
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-11-03 15:47:26 +0530

    ✨improved integer problem template

--- Integer_Problem.R
+++ Integer_Problem.R
@@ -7,11 +7,17 @@
 # The length of this vector will therefore be the number of integer variables.
 #---------------------------------------------------------------------------------------------------------------------------------------
 
+# Maximize 4x-y+3z
+# Subject to  2x + y + z <= 4
+#             x + y + z >= 5
+#             4x + 3y + 2z >= 0
 
-library(lpSolve) 
-f.obj=c(4,-1,3)
-f.con=matrix(c(2,1,1,1,1,1,4,3,2),nrow=3,byrow=TRUE)
-f.dir=c("<=",">=","<=")
-f.rhs=c(8,2,15)
-ans=lp("max", f.obj, f.con, f.dir, f.rhs, int.vec = 1:3)
-ans$solution
+# -------------------------------------------
+
+library(lpSolve)
+f.obj=c(4,-1,3) # objective function constraints 
+f.con=matrix(c(2,1,1,1,1,1,4,3,2),byRow=TRUE,nrow=3)  # LHS in form of matrix
+f.dir=c("<=",">=",">=")  # all signs
+f.rhs=c(4,5,0)  # RHS
+answer=lp("max",f.obj,f.con,f.dir,f.rhs,int.vec=1:3) # computation || you can change max to min depending on question
+answer$solution # final result
